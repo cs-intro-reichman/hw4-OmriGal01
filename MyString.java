3a123f5f8e946a78037948373817315cf791fbf7
Omri Gal <omri.gal01@post.runi.ac.il>
2024-11-27 23:25:09 +0200

    first attempt

--- MyString.java
+++ MyString.java
@@ -18,13 +18,40 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String lower = "";
+        char ch;
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+            if (ch >= 65 && ch <= 90) {
+                lower += (char) (ch + 32);
+            }
+            else {
+                lower += ch;
+            }
+        }
+        return lower;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+        if (str1.length() == 0) {
+            System.out.println("flag1");
+            return false;
+        }
+        if (str2.length() == 0) {
+            System.out.println("flag2");
+            return true;
+        }
+        int firstIteration = str1.indexOf(str2.charAt(0));
+        if (firstIteration == -1) {
+            System.out.println("flag3");
+            return false;
+        }
+        for (int i = 0; i < str2.length(); i++) {
+            if (str2.charAt(i) != str1.charAt(firstIteration + i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
